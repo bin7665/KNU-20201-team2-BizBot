40e1712e5a1c8533828fa1fe924604a7269780dc
qkrwoghk <akavmvkdldj1@naver.com>
2020-06-08 20:50:41 +0900

    20200608 20:50 version update Temp MainPage

--- MainPage(testing)/section2_graph2.js
+++ MainPage(testing)/section2_graph2.js
@@ -7,33 +7,37 @@ var s2g2Width = 450,
 var radius = Math.min(s2g2Width, s2g2Height) / 2 - s2g2Margin
 
 // append the svg object to the div called 'my_dataviz'
-var svg = d3.select("#section2_graph2")
+var s2g2Svg = d3.select("#section2_graph2")
   .append("svg")
     .attr("width", s2g2Width)
     .attr("height", s2g2Height)
   .append("g")
     .attr("transform", "translate(" + s2g2Width / 2 + "," + s2g2Height / 2 + ")");
 
+
 //Read the data
 d3.csv("https://raw.githubusercontent.com/bin7665/KNU-20201-team2-BizBot/master/static/data/depart_data.csv",
-  // When reading the csv, I must format variables:
-  function(d){
-    // const textdecoder = new TextDecoder("iso-2022-cn-ext");//"\ufeff"+
-    // console.log(textdecoder.decode(d.Department))
-    return { depart : d.depart, value : +d.value }
-  },
-
   // Now I can use this dataset:
   function(data) {
+    // When reading the csv, I must format variables:
+    var Stringdata = '{ ';
+        data.map(function(d){
+          Stringdata += `${JSON.stringify(d.depart)} : ${JSON.stringify(d.value)}, `;
+        })
+        Stringdata = Stringdata.substr(0, Stringdata.length-2);
+        Stringdata += ' }';
+
+    var dataReady = JSON.parse(Stringdata)
+
     // set the color scale
     var color = d3.scaleOrdinal()
-      .domain(data.map(function(d){return d.value}))
+      .domain(dataReady)
       .range(d3.schemeSet2);
 
     // Compute the position of each group on the pie:
     var pie = d3.pie()
-      .value(function(d) {return d.value; })
-    var data_ready = pie(d3.entries(data))
+      .value(function(d) {return d.value;})
+    var data_ready = pie(d3.entries(dataReady))
     // Now I know that group A goes from 0 degrees to x degrees and so on.
 
     // shape helper to build arcs:
@@ -42,24 +46,24 @@ d3.csv("https://raw.githubusercontent.com/bin7665/KNU-20201-team2-BizBot/master/
       .outerRadius(radius)
 
     // Build the pie chart: Basically, each part of the pie is a path that we build using the arc function.
-    svg
+    s2g2Svg
       .selectAll('mySlices')
       .data(data_ready)
       .enter()
       .append('path')
         .attr('d', arcGenerator)
-        .attr('fill', function(d){ return(color(d.data.depart)) })
+        .attr('fill', function(d){ return(color(d.data.key)) })
         .attr("stroke", "black")
         .style("stroke-width", "2px")
         .style("opacity", 0.7)
 
     // Now add the annotation. Use the centroid method to get the best coordinates
-    svg
+    s2g2Svg
       .selectAll('mySlices')
       .data(data_ready)
       .enter()
       .append('text')
-      .text(function(d){ return "grp " + d.data.depart})
+      .text(function(d){ return d.data.key})
       .attr("transform", function(d) { return "translate(" + arcGenerator.centroid(d) + ")";  })
       .style("text-anchor", "middle")
       .style("font-size", 17)
---
+++ MainPage(testing)/section2_graph3.js
@@ -0,0 +1,70 @@
+// set the dimensions and margins of the graph
+var s2g3Width = 450,
+    s2g3Height = 450,
+    s2g3Margin = 40;
+
+// The radius of the pieplot is half the width or half the height (smallest one). I subtract a bit of margin.
+var radius = Math.min(s2g3Width, s2g3Height) / 2 - s2g3Margin
+
+// append the svg object to the div called 'my_dataviz'
+var s2g3Svg = d3.select("#section2_graph3")
+  .append("svg")
+    .attr("width", s2g3Width)
+    .attr("height", s2g3Height)
+  .append("g")
+    .attr("transform", "translate(" + s2g3Width / 2 + "," + s2g3Height / 2 + ")");
+
+
+//Read the data
+d3.csv("https://raw.githubusercontent.com/bin7665/KNU-20201-team2-BizBot/master/static/data/category_inquiry.csv",
+  // Now I can use this dataset:
+  function(data) {
+    // When reading the csv, I must format variables:
+    var Stringdata = '{ ';
+        data.map(function(d){
+          Stringdata += `${JSON.stringify(d.category)} : ${JSON.stringify(d.value)}, `;
+        })
+        Stringdata = Stringdata.substr(0, Stringdata.length-2);
+        Stringdata += ' }';
+
+    var dataReady = JSON.parse(Stringdata)
+
+    // set the color scale
+    var color = d3.scaleOrdinal()
+      .domain(dataReady)
+      .range(d3.schemeSet2);
+
+    // Compute the position of each group on the pie:
+    var pie = d3.pie()
+      .value(function(d) {return d.value;})
+    var data_ready = pie(d3.entries(dataReady))
+    // Now I know that group A goes from 0 degrees to x degrees and so on.
+
+    // shape helper to build arcs:
+    var arcGenerator = d3.arc()
+      .innerRadius(0)
+      .outerRadius(radius)
+
+    // Build the pie chart: Basically, each part of the pie is a path that we build using the arc function.
+    s2g3Svg
+      .selectAll('mySlices')
+      .data(data_ready)
+      .enter()
+      .append('path')
+        .attr('d', arcGenerator)
+        .attr('fill', function(d){ return(color(d.data.key)) })
+        .attr("stroke", "black")
+        .style("stroke-width", "2px")
+        .style("opacity", 0.7)
+
+    // Now add the annotation. Use the centroid method to get the best coordinates
+    s2g3Svg
+      .selectAll('mySlices')
+      .data(data_ready)
+      .enter()
+      .append('text')
+      .text(function(d){ return `${d.data.key} : ${d.data.value}`})
+      .attr("transform", function(d) { return "translate(" + arcGenerator.centroid(d) + ")";  })
+      .style("text-anchor", "middle")
+      .style("font-size", 17)
+})
